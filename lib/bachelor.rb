91511c97d6932ee4ce02ea0dc25f2c120db2d36b
fut0k0 <cccabell@icloud.com>
2019-04-24 09:15:18 -0700

    Passing lab

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,37 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+	roster = data.fetch("#{season.to_sym}")
+	roster.each do |person|
+		return person["name"].split(" ")[0] if person["status"] == "Winner"
+	end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+	data.each do |show, roster|
+		roster.each do |person|
+			return person["name"] if person["occupation"] == occupation
+		end	
+	end	
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+	count = 0
+	data.each do |show, roster|
+		roster.each {|person| count += 1 if person["hometown"] == hometown}
+	end	
+	count
 end
 
 def get_occupation(data, hometown)
-  # code here
+	data.each do |show, roster|
+		roster.each do |person|
+			return person["occupation"] if person["hometown"] == hometown
+		end	
+	end	
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+	total_age = 0
+	roster = data.fetch("#{season.to_sym}")
+	roster.each {|person| total_age += person["age"].to_f}
+	(total_age / roster.size).round
 end
